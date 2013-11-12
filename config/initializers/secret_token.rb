f7db226dd271cf7b655dc54a350a71f0770a537c
Guitar Shed <guitarshednet@gmail.com>
2013-11-11 22:05:29 -0800

    Created a rails application and added status update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Guitar::Application.config.secret_token = '86b688b3720efe01fffe83fae289412a25e27ba6f0386cef1aba52e5aed7d311940387b37568626b085d1368ef8f2dd3cb5871c2e19ea2a41d8978bb754a3db2'
