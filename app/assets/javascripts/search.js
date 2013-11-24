a58c753b0772b59c4bbc97f9f7c06808cc40a97c
Guitar Shed <guitarshednet@gmail.com>
2013-11-23 22:37:36 -0800

    added video div on lesson click

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -9,7 +9,8 @@ function search() {
   var request = gapi.client.youtube.search.list({
     q: q,
     part: 'snippet',
-    order: 'relevance'
+    order: 'relevance',
+    type: 'video'
   });
 
   $('#searchContainer').html('');
@@ -19,38 +20,48 @@ function search() {
   
     for(var i = 0, len = response.result.items.length; i < len; i++){
       console.log(response.result.items[i]);
-      var resultId = response.result.items[i].id.videoId;
-      var resultTitle = response.result.items[i].snippet.title;
-      var resultImg = response.result.items[i].snippet.thumbnails.medium.url;
-      var divId = "ytplayer" + i;
+      this.resultId = response.result.items[i].id.videoId;
+      this.resultTitle = response.result.items[i].snippet.title;
+      this.resultImg = response.result.items[i].snippet.thumbnails.medium.url;
+      this.divId = "ytplayer" + i;
      
-      showDetails(resultTitle, resultImg, divId);
-
-
+       
+      showDetails(resultTitle, resultImg, divId, resultId);
+      buildEvent(resultId, divId);
     }
 
-     
-    function showDetails(resultTitle, resultImg, divId){
 
-      $('#searchContainer').append('<div id="' + divId + '" " class="lesson span10">></div>');
-      $("#" + divId).html("</div><h2 class='pull-left'>" + resultTitle + "</h2><img src='" + resultImg + "' class='pull-right'>");
+
+    function showDetails(resultTitle, resultImg, divId, videoId){
+
+      $('#searchContainer').append('<div id="' + divId + '" " class="lesson span10"></div>');
+      $("#" + divId).html("</div><h4 class='pull-left'>" + resultTitle + "</h4><img src='" + resultImg + "' class='pull-right'>");
+      
     }
 
-   function playResults(videoId, divId){
-    player = new YT.Player(divId, {
-        height: '390',
-        width: '640',
-        videoId: videoId
-      });
-
-}
+ 
     
    
    
 
   });
 
+  function buildEvent(videoId, divId){
+    $('#' + divId).one("click",function(){
+     
+      $('#' + divId).append('<div class="videoContainer_' + videoId +'"></div>');
 
+        playResults(videoId, divId);
+    });
+  }
 }
 
 
+
+  function playResults(videoId, videoDiv){
+    $('.videoContainer_' + videoId +'').html('<iframe id="ytplayer" type="text/html" width="640" height="360" src="https://www.youtube.com/embed/' + videoId + '" frameborder="0" allowfullscreen>');
+      $('.videoContainer_' + videoId +'').click(function(){
+        $('.videoContainer_' + videoId +'').remove()
+        
+      })
+    }
