62e3c229001bb49dc7edc648e64384eea018d987
Guitar Shed <guitarshednet@gmail.com>
2013-11-23 19:15:37 -0800

    added Jquery formatting for search query

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -8,13 +8,49 @@ function search() {
   var q = $('#query').val();
   var request = gapi.client.youtube.search.list({
     q: q,
-    part: 'snippet'
+    part: 'snippet',
+    order: 'relevance'
   });
 
+  $('#searchContainer').html('');
+
   request.execute(function(response) {
-    var str = JSON.stringify(response.result);
-    $('#search-container').html(str);
+    console.log(response.result);
+  
+    for(var i = 0, len = response.result.items.length; i < len; i++){
+      console.log(response.result.items[i]);
+      var resultId = response.result.items[i].id.videoId;
+      var resultTitle = response.result.items[i].snippet.title;
+      var resultImg = response.result.items[i].snippet.thumbnails.medium.url;
+      var divId = "ytplayer" + i;
+     
+      showDetails(resultTitle, resultImg, divId);
+
+
+    }
+
+     
+    function showDetails(resultTitle, resultImg, divId){
+
+      $('#searchContainer').append('<div id="' + divId + '" " class="lesson span10">></div>');
+      $("#" + divId).html("</div><h2 class='pull-left'>" + resultTitle + "</h2><img src='" + resultImg + "' class='pull-right'>");
+    }
+
+   function playResults(videoId, divId){
+    player = new YT.Player(divId, {
+        height: '390',
+        width: '640',
+        videoId: videoId
+      });
+
+}
+    
+   
+   
+
   });
 
 
 }
+
+
