f97b574d9fe3c69d4859247918c76dc630b10631
Guitar Shed <guitarshednet@gmail.com>
2013-12-27 22:07:37 -0800

    removed tabsearch.rb

--- app/models/tabsearch.rb
+++
@@ -1,9 +0,0 @@
-class tabsearch < tab
-	def listTabs
-		@tabs = Tab.all
-		respond_to do |format|
-      		format.html # index.html.erb
-      		format.json { render json: @statuses }
-   	 	end
-	end
-end
